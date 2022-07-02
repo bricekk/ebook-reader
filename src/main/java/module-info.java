32ec9664f6d99cd88e5f6ad519cd7760597dcc92
Brice <briceklog@gmail.com>
2022-07-02 10:09:41 +0100

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.reader.reader {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.reader.reader to javafx.fxml;
+    exports com.reader.reader;
+}
